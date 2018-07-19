a33e30c4e13129e5a922137c338568d0a3f4d527
Siddharth M <siddharth.m98@gmail.com>
2018-07-19 17:43:06 +0530

    Uses Permissions granting Feature

--- app/src/main/java/com/siddharthm/gogps/MainActivity.java
+++ app/src/main/java/com/siddharthm/gogps/MainActivity.java
@@ -1,16 +1,23 @@
 package com.siddharthm.gogps;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.karan.churi.PermissionManager.PermissionManager;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth auth;
     private FirebaseUser user;
+    PermissionManager manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
         user = auth.getCurrentUser();
         if (user == null){
             setContentView(R.layout.activity_main);
+            manager = new PermissionManager() {};
+            manager.checkAndRequestPermissions(this);
         }else {
             Intent myIntent = new Intent(MainActivity.this,MyNavigationActivity.class);
             startActivity(myIntent);
@@ -26,6 +35,16 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        manager.checkResult(requestCode,permissions,grantResults);
+        ArrayList<String> deniedPermission = manager.getStatus().get(0).denied;
+        if (deniedPermission.isEmpty()){
+            Toast.makeText(MainActivity.this,"Permissions Enabled",Toast.LENGTH_SHORT).show();
+        }
+    }
+
     public void goToLogin(View v){
         Intent myIntent = new Intent(MainActivity.this,LoginActivity.class);
         startActivity(myIntent);
